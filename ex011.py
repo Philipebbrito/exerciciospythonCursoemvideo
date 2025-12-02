41b9472a35b895426b9ad3452d375c2a75d516fb
Philipebbrito <philipe.borges@gmail.com>
2025-12-01 23:09:08 -0300

    Delete ex011.py

--- ex011.py
+++
@@ -1,6 +0,0 @@
-larg = float(input('Largura da parede: '))
-alt = float(input('Altura da parede: '))
-area = larg * alt
-print('Sua parede tem a dimensao de {}mt X {}mt e sua área é de {}mt²'.format(larg, alt, area))
-tinta = area / 2
-print('Para pintar essa parede voce vai precisar de {}lts de tinta'.format(tinta))
