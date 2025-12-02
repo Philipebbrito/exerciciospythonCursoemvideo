b4f6cbfbee86690014337749ee2b879174865bb6
Philipebbrito <philipe.borges@gmail.com>
2025-12-01 23:09:24 -0300

    Delete ex013.py

--- ex013.py
+++
@@ -1,3 +0,0 @@
-salario = float(input('Qual o salário do funcionário: R$'))
-novo = salario + (salario * 5 / 100)
-print('O salário que era R${:.2f}, com o aumento de 5% passa a receber R${:.2f}'.format(salario,novo))
